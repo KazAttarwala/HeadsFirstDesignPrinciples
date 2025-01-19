e52a6bde0b570eae4bbf7e1679ced6489e5dc0cd
Qais Attarwala <qais.attarwala0@yahoo.com>
2025-01-19 10:14:09 -0500

    assembly info

--- DuckApp/obj/Debug/net9.0/DuckApp.AssemblyInfo.cs
+++ DuckApp/obj/Debug/net9.0/DuckApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DuckApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d8a2b38309126a4fb23ad99f9109341d4727dd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DuckApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DuckApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
